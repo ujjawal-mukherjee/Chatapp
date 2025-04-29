5ca86741ddb3589da40c63a04316a36bb0b1b490
ujjawal-mukherjee <ujjawalkumarmukherjee335@gmail.com>
2025-04-29 19:45:41 +0530

    Update constants.js

--- Context/constants.js
+++ Context/constants.js
@@ -1,5 +1,5 @@
 import chatAppJSON from './ChatApp.json';
-export const ChatAppAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const ChatAppAddress = "";
 export const ChatAppABI = chatAppJSON.abi;
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
 //0x5FbDB2315678afecb367f032d93F642f64180aa3
